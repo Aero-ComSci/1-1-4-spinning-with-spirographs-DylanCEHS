0febab5385827b0bedd4131151cbe074461f5d2b
DC1309 <157426709+DylanCEHS@users.noreply.github.com>
2024-09-10 13:33:28 -0700

    Create square.py

---
+++ square.py
@@ -0,0 +1,14 @@
+#concentric square
+import turtle as turtl
+painter = turtl.Turtle()
+screen = turtl.Screen()
+screen.setup(width=800, height=800)
+
+painter.speed(0)
+painter.goto(0,0)
+
+
+    
+
+wn = turtl.Screen()
+wn.mainloop()
