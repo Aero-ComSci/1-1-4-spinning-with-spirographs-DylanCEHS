08bffd016d265db12bbb5810153a0dc2207bfddc
DC1309 <157426709+DylanCEHS@users.noreply.github.com>
2024-09-10 17:43:52 -0700

    Update square.py

--- square.py
+++ square.py
@@ -1,14 +1,38 @@
-#concentric square
-import turtle as turtl
-painter = turtl.Turtle()
-screen = turtl.Screen()
+import turtle as trtl
+import random
+
+painter = trtl.Turtle()
+screen = trtl.Screen()
 screen.setup(width=800, height=800)
 
 painter.speed(0)
-painter.goto(0,0)
+painter.penup()
+painter.goto(-15,15)
+painter.pendown()
+painter.pensize(4)
+
+x=30
+
+colors = ["red", "orange", "yellow", "blue", "green", "purple"]
+
+while True:
+        painter.pencolor(random.choice(colors))
+        for j in range(4):
+            painter.forward(x)
+            painter.right(90)
+        if x > 800:
+            break 
+        painter.penup()
+        painter.backward(30)  
+        painter.right(90)  
+        painter.backward(30) 
+        painter.left(90)
+        painter.pendown()
+        x=x+60
 
 
-    
 
-wn = turtl.Screen()
+
+
+wn = trtl.Screen()
 wn.mainloop()
